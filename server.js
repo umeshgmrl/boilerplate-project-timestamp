e820cd7c57c1a5797fc2cf0ca226dd4f3173630f
Bhumesh <bhumesh@Pavans-Mac-mini.local>
2018-12-04 17:09:55 +0530

    implemented the API logic

--- server.js
+++ server.js
@@ -2,31 +2,52 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+	res.json({ greeting: "hello API" });
 });
 
+app.get("/api/timestamp", function(req, res) {
+	res.json(getTime(null));
+});
 
+app.get("/api/timestamp/:date_string", function(req, res) {
+	res.json(getTime(req.params.date_string));
+});
+
+const getTime = date_string => {
+	let date = null;
+	if (!date_string) {
+		date = new Date();
+	} else if (isNaN(date_string)) {
+		date = new Date(date_string);
+	} else {
+		date = new Date(parseInt(date_string));
+	}
+
+	return {
+		unix: date.getTime(),
+		utc: date.toUTCString()
+	};
+};
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+	console.log("Your app is listening on port " + listener.address().port);
 });
